797658f123a1d0793d45fa20e67a4b07c046b79c
PRAY4ENEMY <111301886+PRAY4ENEMY@users.noreply.github.com>
2023-02-03 01:30:39 -0500

    Add files via upload

--- rice.sh
+++ rice.sh
@@ -6,23 +6,41 @@ pacman -Syyu neovim vim i3 picom kitty i3blocks neofetch xwallpapers dmenu
 
 ## apps 
 
-cat $PWD/.picomconf >> /etc/xdg/picom.conf && echo "picom config added"
-cat $PWD/.i3conf >> /etc/i3/config && echo "i3 config added added"
-cat $PWD/.i3bconf >> /etc/i3blocks.conf && echo "i3 blocks config added"
+mv -r wallpapers ~/.config/
+
+sleep 2 
+
+cat /configfiles/picomconf > /etc/xdg/picom.conf && echo "picom config added"
+cat /configfiles/i3conf > /etc/i3/config && echo "i3 config added added"
+cat /configfiles/i3bconf > /etc/i3blocks.conf && echo "i3 blocks config added"
 
 ## utiles 
 
-cat $PWD/.bashrc >> $HOME/.bashrc  && echo "bashrc added"
-cat $PWD/.xinitrc >> $HOME/.xinitrc && echo "xinitrc added"
+
+sleep 2 
+
+cat /configfiles/bashrc > ~/.bashrc  && echo "bashrc added"
+cat /configfiles/xinitrc > ~/.xinitrc && echo "xinitrc added"
 
 ## custom scripts
 
-cp $PWD/duals /usr/local/bin && echo "duals monitor added"
-cp $PWD/mirror /usr/local/bin && echo "mirror monitor added"
-cp $PWD/apps /usr/local/bin && echo "apps added"
-cp $PWD/muse /usr/local/bin && echo "muse added"
+sleep 2 
+
+cp /configfiles/dual /usr/local/bin && echo "duals monitor added"
+cp /configfiles/mirror /usr/local/bin && echo "mirror monitor added"
+cp /configfiles/apps /usr/local/bin && echo "apps added"
+cp /configfiles/muse /usr/local/bin && echo "muse added"
+
+sleep 2
+
+chmod +x /usr/local/bin/dual 
+chmod +x /usr/local/bin/mirror
+chmod +x /usr/local/bin/apps
+chmod +x /usr/local/bin/muse
+chmod +x /usr/local/bin/walls
 
 ## love notes
+sleep 5 
 
 echo "process complete: use 'startx' to start the xorg server and enter into i3wm"
 
@@ -30,7 +48,7 @@ echo "process complete: use 'startx' to start the xorg server and enter into i3w
 
 
 while true; do
-    read -p "Do you wish to experience the PRAY4ENEMY arch rice" yn
+    read -p "Do you wish to experience the PRAY4ENEMY arch rice {y/n}" yn
     case $yn in
         [Yy]* ) rice; break;;
         [Nn]* ) exit;;
